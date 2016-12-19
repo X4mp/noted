5e60429ae4e0335fbee917d7b018f81a593d99f6
Roland <urbano.roland@gmail.com>
2016-12-19 21:10:38 +0100

    added notes view, render view with json input

--- routes/notes.js
+++ routes/notes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Noted' });
+  res.render('notes', { notes: ['note1', 'note2', 'note3'] });
 });
 
 module.exports = router;
